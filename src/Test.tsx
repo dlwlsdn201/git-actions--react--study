2675eb10c766917d62266931f779a5e0618fe757
jinWoo_Lee <dlwlsdn201@naver.com>
2022-07-03 14:40:48 +0900

    test code 생성

---
+++ src/Test.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+
+const Test = () => {
+	const [state, setState] = useState({
+		a: 0,
+		b: 0
+	});
+
+	const setUpdate = (target: 'a' | 'b', value: string) => {
+		console.log('target, value:', target, value);
+		setState({ ...state, [target]: Number(value) });
+	};
+	return (
+		<div>
+			<input type='number' onChange={(e) => setUpdate('a', e.target.value)} />
+			<input type='number' onChange={(e) => setUpdate('b', e.target.value)} />
+		</div>
+	);
+};
+
+export default Test;
